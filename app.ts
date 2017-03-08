28c0ede812b7309f4719bd314b8edf2cf597c1b5
Drake Scott <dascott0204@gmail.com>
2017-03-08 16:55:21 -0600

    using types in functions and the void type

--- app.ts
+++ app.ts
@@ -34,5 +34,31 @@ enum Color {
     Blue = 2// 2
 }
 
-let myColor: Color = Color.Green;
+let myColor: Color = Color.Blue;
 console.log(myColor);
+
+//any
+let car: any = "BMW";
+console.log(car);
+car = { brand: "bmw", series: 3};
+console.log(car);
+
+// functions
+function returnMyName(): string {
+    return myName;
+}
+console.log(returnMyName());
+
+// void
+function sayHello(): void {
+    console.log("hello!");
+    // return myName has an error
+}
+
+// argument types
+function multiply(value1: number, value2: number): number {
+    return value1 * value2;
+}
+
+// console.log(multiply(2, 'Drake'));
+console.log(multiply(2, 5));
