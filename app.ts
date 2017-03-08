1fa42c14e12e5b9c243b55832a51955f94e9be86
Drake Scott <dascott0204@gmail.com>
2017-03-08 17:25:54 -0600

    using functions as types

--- app.ts
+++ app.ts
@@ -62,3 +62,10 @@ function multiply(value1: number, value2: number): number {
 
 // console.log(multiply(2, 'Drake'));
 console.log(multiply(2, 5));
+
+//function types
+let myMultiply: (val1: number, vale2: number) => number;
+// myMultiply = sayHello;
+myMultiply();
+myMultiply = multiply;
+console.log(myMultiply(5, 2));
